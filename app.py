575fecfa162229a6996b69b60d67542d061ef379
Tuğrul <demirozert@gmail.com>
2024-12-20 15:28:39 +0300

    init

---
+++ app.py
@@ -0,0 +1,62 @@
+from pyLoraRFM9x import LoRa, ModemConfig
+# from pyLoraRFM9x.lora import BROADCAST_ADDRESS, FLAGS_REQ_ACK
+from dotenv import load_dotenv
+import requests as req
+from os import getenv
+from time import sleep
+import const
+
+NODES = dict()
+PREV_NODES = dict()
+NUMBER_OF_NODES = 1
+
+def setup():
+    # Configure Lora module
+    lora = LoRa(**const.LORA_CONST, modem_config=ModemConfig.Bw125Cr45Sf128)
+    lora.on_recv = on_recv
+    
+    return lora
+
+def main():
+    lora = setup()
+    try:
+        lora.set_mode_rx()
+        
+        while True:
+            while len(NODES) < NUMBER_OF_NODES:
+                sleep(.2)
+        
+            send_data_to_cloud()
+            PREV_NODES = NODES
+            NODES.clear()
+            sleep(60)
+
+    except KeyboardInterrupt:
+        print("exiting")
+
+def send_data_to_cloud():
+    load_dotenv()
+    api_key = getenv("API_KEY")
+    if not api_key:
+        raise ValueError("API key not found")
+    
+    try:
+        for node, data in NODES.items():
+            res = req.get(const.URL+api_key, params=data)
+            res.raise_for_status()
+            print(f"node:{node} send data {data}")
+
+    except req.exceptions.RequestException as e:
+        print("Error sending data:", e)
+    
+def on_recv(message):
+    nums = list(map(int, message.message.split(":")))
+    data = {"field1": nums[0],
+            "field2": nums[1],
+            "field3": nums[3]}
+    NODES[message.header_from] = data
+    print(message)
+
+if __name__ == '__main__':
+    main()
+
---
+++ const.py
@@ -0,0 +1,16 @@
+LORA_CONST = {
+    "spi_channel": 0,
+    "spi_port": 0,
+    "interrupt_pin": 5,
+    "my_address": 1,
+    "reset_pin": 25,
+    "tx_power": 14,
+    "acks": False,
+    "freq": 433,
+    "receive_all": True,
+    "crypto": None,
+    "default_mode": 0
+}
+
+URL = "https://api.thingspeak.com/update?api_key="
+
