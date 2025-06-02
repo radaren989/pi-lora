7eaf1c35181be8e2c13376f4efb7fd97bfc33dc3
Tuğrul <demirozert@gmail.com>
2025-06-02 18:21:03 +0300

    multiple node

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from pyLoraRFM9x import LoRa, ModemConfig
-# from pyLoraRFM9x.lora import BROADCAST_ADDRESS, FLAGS_REQ_ACK
 from dotenv import load_dotenv
+from pyLoraRFM9x.lora import BROADCAST_ADDRESS
 import requests as req
 from os import getenv
 from time import sleep
@@ -8,7 +8,23 @@ import const
 
 NODES = dict()
 PREV_NODES = dict()
-NUMBER_OF_NODES = 1
+NUMBER_OF_NODES = 4
+
+#ADDRESSES
+#  1 -> gateway
+#  2 -> first data node
+#  3 -> first water node
+#  4 -> second data node
+#  5 -> second water node
+#  255 -> boradcast 
+
+load_dotenv()
+API_KEY = getenv("API_KEY")
+
+API_MAP = {0:getenv("API_KEY1"),
+           1:getenv("API_KEY1"),
+           2:getenv("API_KEY2"),
+           3:getenv("API_KEY2")}
 
 def setup():
     # Configure Lora module
@@ -22,36 +38,54 @@ def main():
     try:
         while True:
             print("Waking up...")
-            lora.set_mode_rx()
-            while len(NODES) < NUMBER_OF_NODES:
-                sleep(.1)
-            
+            wait_for_all(lora)
+
+            #may need to modify on_recv func because I get two tyes of msg now
+            print("Sending waiting time")
+            send_wait_time(lora, 15)
 
             lora.set_mode_sleep()
             send_data_to_cloud()
+
             PREV_NODES = NODES.copy()
             NODES.clear()
+
             print("Sleeping...")
             sleep(20)
 
     except KeyboardInterrupt:
         print("exiting")
 
+#Waits for all nodes to send msg
+def wait_for_all(lora:LoRa):
+    lora.set_mode_rx()
+    
+    while len(NODES) < NUMBER_OF_NODES:
+        sleep(.1)
+
+#Sends seconds as broadscast for nodes to wait
+def send_wait_time(lora:LoRa, seconds:int) -> None:
+    lora.set_mode_tx()
+
+    for _ in range(3):
+        lora.send_to_wait(seconds, BROADCAST_ADDRESS)
+        sleep(.1)
+
+#Sends data to cloud
 def send_data_to_cloud():
-    load_dotenv()
-    api_key = getenv("API_KEY")
-    if not api_key:
+    if not API_KEY:
         raise ValueError("API key not found")
     
     try:
         for node, data in NODES.items():
-            res = req.get(const.URL+api_key, params=data)
+            res = req.get(const.URL+API_KEY, params=data)
             res.raise_for_status()
             print(f"node:{node} send data {data}")
 
     except req.exceptions.RequestException as e:
         print("Error sending data:", e)
-    
+
+#Decodes messages
 def on_recv(message):
     global NODES
     try:
